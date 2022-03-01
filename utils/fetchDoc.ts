01f9f37f8c2d19e0c4daa0ac03cdf712c2456a8a
Aman Thakur <madebyaman@gmail.com>
2022-03-02 01:48:05 +0530

    checking ssh connection

--- utils/fetchDoc.ts
+++ utils/fetchDoc.ts
@@ -4,6 +4,7 @@ import db from '../firebase';
 /**
  * Function to fetch a document with a url. It should return a document snapshot if everything went well.
  * @param url The url of document to be fetched.
+ * Are we even using it?
  */
 export const fetchDoc = async (url: string) => {
   const docRef = doc(db, url);

9de6c24deb7270a14996a9fbc62345318c52d05c
Aman Thakur <madebyaman@gmail.com>
2022-03-10 22:31:32 +0530

    designing dashboard page AGAIN

--- components/Dashboard/DashboardLayout.tsx
+++ components/Dashboard/DashboardLayout.tsx
@@ -1,17 +1,12 @@
-import { Box, Container, Flex, Heading, Image, Text } from '@chakra-ui/react';
+import { Box, Container } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
-import { useContext, useEffect, useState } from 'react';
-import { FiBook, FiLogOut, FiUser } from 'react-icons/fi';
-import { ImUser } from 'react-icons/im';
+import { useContext } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 
-import { Tab } from '.';
-import { signout } from '../../utils/auth';
 import { AuthContext } from '../Auth';
-import { ResendVerificationEmail } from '../Layout/ResendVerificationEmail';
-import { UserProfile } from '../../@types';
 import { ErrorFallback } from '../Error';
-import { getCurrentUserProfile } from '../Profile';
+import { Nav } from '../nav';
+import { BookCards } from './BookCards';
 
 export const DashboardLayout = ({
   children,
@@ -19,86 +14,17 @@ export const DashboardLayout = ({
   children: React.ReactNode;
 }) => {
   const router = useRouter();
-  const [user, setUser] = useState<UserProfile | undefined>();
   const isDashboardActive = router.pathname === '/dashboard'; // To figure whether the page is dashboard or profile page.
   const userInfo = useContext(AuthContext);
 
-  useEffect(() => {
-    const loadUser = async () => {
-      try {
-        const currentUser = await getCurrentUserProfile();
-        if (currentUser) {
-          setUser(currentUser);
-        }
-      } catch (e) {
-        throw e;
-      }
-    };
-    loadUser();
-  }, []);
-
   return (
-    <ErrorBoundary
-      FallbackComponent={ErrorFallback}
-      onReset={() => {
-        setUser(undefined);
-      }}
-      resetKeys={[user]}
-    >
-      {!userInfo?.emailVerified && (
-        <Box backgroundColor={'#FC8181'} textAlign="center">
-          <Container py="2">
-            Your email is not verified. <ResendVerificationEmail />
-          </Container>
-        </Box>
-      )}
-      <Box backgroundColor={'#EDF2F7'}>
-        <Container maxW="container.lg" pt={'16'}>
-          <Flex justify="center" align={'center'}>
-            {user && user.photo ? (
-              <Image
-                borderRadius={'full'}
-                alt={user.name}
-                src={user.photo}
-                w="100px"
-                h="100px"
-              />
-            ) : (
-              <ImUser fontSize={'50px'} />
-            )}
-            <Box ml={8}>
-              <Heading as="h1">Hello {user?.name || 'there'}</Heading>
-              <Text ml={1} mt={'2'}>
-                {isDashboardActive
-                  ? 'Here are book notes'
-                  : 'Update your profile here'}
-              </Text>
-            </Box>
-          </Flex>
-          <Flex mt={12} justify={'center'}>
-            <Tab
-              icon={<FiBook />}
-              active={isDashboardActive}
-              onClick={() => router.push('/dashboard')}
-            >
-              Book Notes
-            </Tab>
-            <Tab
-              icon={<FiUser />}
-              active={!isDashboardActive}
-              onClick={() => router.push('/dashboard/profile')}
-            >
-              Profile
-            </Tab>
-            <Tab icon={<FiLogOut />} onClick={signout}>
-              Logout
-            </Tab>
-          </Flex>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <Box>
+        <Container maxW="container.lg">
+          <Nav showFullNav={true} />
         </Container>
       </Box>
-      <Container maxW="container.lg" mt={16} mb={12}>
-        {children}
-      </Container>
+      <BookCards />
     </ErrorBoundary>
   );
 };
--- components/nav.tsx
+++ components/nav.tsx
@@ -1,11 +1,17 @@
-import { Image, Flex, Link as ChakraLink } from '@chakra-ui/react';
+import {
+  Image,
+  Flex,
+  Link as ChakraLink,
+  Spacer,
+  Text,
+} from '@chakra-ui/react';
 import Link from 'next/link';
 import { useContext } from 'react';
 import { signout } from '../utils/auth';
 
 import { AuthContext } from './Auth';
 
-export const Nav = () => {
+export const Nav = ({ showFullNav }: { showFullNav?: boolean }) => {
   const user = useContext(AuthContext);
 
   return (
@@ -20,12 +26,85 @@ export const Nav = () => {
             height="74px"
           />
         </Link>
+        <Spacer />
+        {user && showFullNav && (
+          <>
+            <Link href={`/dashboard`} passHref>
+              <ChakraLink
+                fontSize={'md'}
+                color="gray.600"
+                borderBottom={'2px'}
+                borderColor="transparent"
+                transition={'all 200ms ease-out'}
+                _hover={{
+                  textDecoration: 'none',
+                  borderColor: 'primary.700',
+                  color: 'primary.400',
+                }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  style={{
+                    width: '22px',
+                    height: '22px',
+                  }}
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z"
+                  />
+                </svg>
+                Dashboard
+              </ChakraLink>
+            </Link>
+            <Link href="/profile" passHref>
+              <ChakraLink
+                fontSize={'md'}
+                color="gray.600"
+                borderBottom={'2px'}
+                borderColor="transparent"
+                transition={'all 200ms ease-out'}
+                _hover={{
+                  textDecoration: 'none',
+                  borderColor: 'primary.700',
+                  color: 'primary.400',
+                }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  style={{
+                    width: '22px',
+                    height: '22px',
+                  }}
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                  />
+                </svg>
+                Profile
+              </ChakraLink>
+            </Link>
+          </>
+        )}
         <Link href={user ? '#' : '/signin'} passHref>
           <ChakraLink
-            onClick={() => signout()}
-            fontSize={'md'}
+            onClick={() => user && signout()}
             color="gray.600"
             borderBottom={'2px'}
+            display="flex"
+            flexDir={'column'}
+            alignItems={'center'}
             borderColor="transparent"
             transition={'all 200ms ease-out'}
             _hover={{
@@ -34,7 +113,44 @@ export const Nav = () => {
               color: 'primary.400',
             }}
           >
-            {user ? 'Sign out' : 'Sign in'}
+            {user ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                style={{
+                  width: '22px',
+                  height: '22px',
+                }}
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                style={{
+                  width: '22px',
+                  height: '22px',
+                }}
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
+                />
+              </svg>
+            )}
+            <Text fontSize="md">{user ? 'Sign out' : 'Sign in'}</Text>
           </ChakraLink>
         </Link>
       </Flex>
